fe7cd8afab16c3d45cc9693f97d65e056fc388a0
Chris Ki <chriski777@berkeley.edu>
2017-06-19 16:05:50 -0400

    Added FanoFactor analysis script

---
+++ Functions/fanoFactorAnalysis.m
@@ -0,0 +1,10 @@
+function fanoFactor = fanoFactorAnalysis(SPKC)
+%Chris Ki, June 2017, Gittis Lab
+%fanoFactorAnalysis: Performs fano Factor analysis to analyze self-similarity
+%   over a range of time scales. The fano factor is defined as the variance
+%   of the spike count divided by its mean.
+%
+%   SPKC = A single spike train vector where each entry is the timepoint at
+%       occurence of a spike
+   fanoFactor = 1;
+end
